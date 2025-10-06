c2d5e02dfa8d730952b027c1f6885858a0dbf47d
Florent Dujardin--Duribreux <florent.dujardin-duribreux@epitech.eu>
2025-10-06 18:13:27 +0200

    refactor: look of navbar

--- src/components/navbar/Category.jsx
+++ src/components/navbar/Category.jsx
@@ -4,9 +4,9 @@ import { useRef, useEffect } from 'react'
 function Category({ category }) {
   return (
     <a href={category[1]}>
-        <div className='mr-5'>
+        <div className='mr-2'>
             <div className='text-category-bg bg-[#1E2A38]'>
-                <li className='text-white  font-bold text-category'>
+                <li className='text-white  font-bold text-category pr-3 pb-1 pt-1 pl-3'>
                     {category[0]}
                 </li>
             </div>
