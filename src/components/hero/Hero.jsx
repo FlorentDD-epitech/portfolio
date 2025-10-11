507877e80011a06afbb2750e23a4349dbcca4cd8
Florent Dujardin Duribreux <florent.dujardin-duribreux@epitech.eu>
2025-10-11 02:35:30 +0200

    refactor: no gap with justify around

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ import hero_image from '../../assets/hero_image.png'
 function Hero() {
   return (
     <>
-      <div id='Hero' className='flex flex-row items-center justify-around h-screen'>
+      <div id='Hero' className='flex flex-row items-center justify-center h-screen gap-40'>
         <div className='flex justify-center items-center size-140'>
             <div className='flex flex-col justify-center'>
                 <h3 className='text-5xl font-bold text-[#1E2A38] text-justify'>Hello 👋,</h3>
